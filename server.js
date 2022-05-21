7f0d5e48cdfe4d2756a8d10be518b130840299ba
CamiAlderete <49692059+CamiAlderete@users.noreply.github.com>
2022-05-21 01:58:33 -0400

    permiso para recibir requests del front

--- server.js
+++ server.js
@@ -5,10 +5,20 @@ const app = express();
 const db = require("./app/models");
 db.sequelize.sync();
 
+//la direccion del front
 var corsOptions = {
-    origin: "http://localhost:9091"
+    //origin: "http://localhost:9091"
+    origin: "http://localhost:4200"
 };
 app.use(cors(corsOptions));
+
+/*
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "YOUR-DOMAIN.TLD"); // update to match the domain you will make the request from
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+* */
 // parse requests of content-type - application/json
 app.use(bodyParser.json());
 
