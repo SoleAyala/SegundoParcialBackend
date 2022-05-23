99e8e3ff4d6c79a13159f74685dc97d1d76ff49a
monica.auler <monica.auler@konecta.com.py>
2022-05-23 11:36:09 -0400

    Update configuraciones a como estaban antes

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,7 +1,7 @@
 module.exports = {
     HOST: "localhost",
     USER: "postgres",
-    PASSWORD: "postgres", //cambiar
+    PASSWORD: "admin", //cambiar
     DB: "bdpwb-p2", //crear db
     dialect: "postgres",
     pool: {
